5c7abb6d5ef20bf79219b398cf9de16bd55755de
Abby <168043915+abbyhayd@users.noreply.github.com>
2026-09-08 12:21:14 -0700

    Switch GitHub projects mode to manual and update list
    Changed GitHub projects mode from 'automatic' to 'manual' and updated the list of projects to display.

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -14,7 +14,7 @@ const CONFIG = {
     github: {
       display: true, // Display GitHub projects?
       header: 'Projects',
-      mode: 'automatic', // Mode can be: 'automatic' or 'manual'
+      mode: 'manual', // Mode can be: 'automatic' or 'manual'
       automatic: {
         sortBy: 'stars', // Sort projects by 'stars' or 'updated'
         limit: 6, // How many projects to display.
@@ -25,7 +25,7 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: [], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['abbyhayd/OneWayOut', 'MissionToPsyche-Platinum/platinum_24g_web_game-su','abbyhayd/Protect-the-Planet', 'abbyhayd/HackSU2026', 'abbyhayd/GameStatsBot', 'abbyhayd/BookSale'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
